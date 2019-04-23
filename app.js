43b81f884fcabe55cede22574b93491ac62575ea
daiyu <daiyu@sunlands.com>
2019-04-23 16:01:18 +0800

    log4

--- app.js
+++ app.js
@@ -4,20 +4,40 @@ const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
-
+const logUtil = require('./utils/log_utils');
 // error handler
 onerror(app)
 
+
+// logger
+app.use(async (ctx, next) => {
+  //响应开始时间
+  const start = new Date();
+  //响应间隔时间
+  var ms;
+  try {
+    //开始进入到下一个中间件
+    await next();
+
+    ms = new Date() - start;
+    //记录响应日志
+    logUtil.logResponse(ctx, ms);
+
+  } catch (error) {
+    ms = new Date() - start;
+    //记录异常日志
+    logUtil.logError(ctx, error, ms);
+  }
+});
+
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
-app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
